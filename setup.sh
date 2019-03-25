b1749bc4b496885a796e11a0a130cb1db9e8791d
vibhujawa <vibhujawa@gmail.com>
2019-03-25 16:22:43 -0700

    pointed to vibhu_s fork of the make file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#git clone https://github.com/NVIDIA/gdrcopy.git
+git clone https://github.com/VibhuJawa/ucx-dev-env.git
+# git clone https://github.com/TomAugspurger/ucx-dev-env.git
+cd ucx-dev-env
 git clone https://github.com/Akshay-Venkatesh/ucx-py
-git clone https://github.com/TomAugspurger/ucx-dev-env.git
-
+cd ..
 docker build -t docker_ucx .
