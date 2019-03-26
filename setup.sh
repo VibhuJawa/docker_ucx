6913255e85daa5fd328d2dba24ac278342b3e100
Vibhu Jawa <vibhujawa@gmail.com>
2019-03-25 18:15:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/VibhuJawa/ucx-dev-env.git
-git clone https://github.com/TomAugspurger/ucx-dev-env.git
 cd ucx-dev-env
 git clone https://github.com/Akshay-Venkatesh/ucx-py
 cd ..
