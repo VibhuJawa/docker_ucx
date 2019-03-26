7ea03ce2116b484699d3388fc045767922089c10
Vibhu Jawa <vibhujawa@gmail.com>
2019-03-25 18:09:37 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-# git clone https://github.com/VibhuJawa/ucx-dev-env.git
-# git clone https://github.com/TomAugspurger/ucx-dev-env.git
-# cd ucx-dev-env
-# git clone https://github.com/Akshay-Venkatesh/ucx-py
-`cd ..
+git clone https://github.com/VibhuJawa/ucx-dev-env.git
+git clone https://github.com/TomAugspurger/ucx-dev-env.git
+cd ucx-dev-env
+git clone https://github.com/Akshay-Venkatesh/ucx-py
+cd ..
 docker build -t docker_ucx .
