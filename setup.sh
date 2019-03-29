0e48d0b3cee00c3a63f5cf2b29e267a2694a7537
Vibhu Jawa <vibhujawa@gmail.com>
2019-03-29 09:36:04 -0700

    added branch with tcp fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/VibhuJawa/ucx-dev-env.git
 cd ucx-dev-env
-git clone https://github.com/Akshay-Venkatesh/ucx-py
+git clone https://github.com/Akshay-Venkatesh/ucx-py  --branch /topic/tcpcm
 cd ..
 docker build -t docker_ucx .
